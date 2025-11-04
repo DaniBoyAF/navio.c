fff9b9bb3943852132ec7bae5fb82b7d1e0d20d3
Daniel <moongames12345@gmail.com>
2025-11-04 09:04:41 -0300

    novos
    funcões tipo batida e outros

--- base.C
+++ base.C
@@ -32,12 +32,26 @@ typedef struct{
     int speed;
     int hp;
 }Boss;
-void mover_em_direcao(Vector2 * origem,Vector destino,float speed ){
-    float dx= destino.X-origem->X;
-    float dy= destino.Y-origem->Y;
-    float dist=sqrt(dx*dx+ dy*dy)
-    if(dist>0){
-        origem->X= (dx/dist)*speed;
-        origem->y= (dy/dist)*speed;
+void mover_inimigo(Inimigo*inimigo, Player* player){
+    float dx=player.pos.x-inimigo->pos.x;
+    float dy = player.pos.y - inimigo->pos.y;
+    float dist = sqrtf(dx * dx + dy * dy);
+    if (dist > 0) {
+        inimigo->pos.x += (dx / dist) * inimigo->speed; // Normaliza direção
+        inimigo->pos.y += (dy / dist) * inimigo->speed;
     }
+}
+void mover_Boss(Boss*boss, Player*player){
+    float dx=player.pos.x-Boss->pos.x;
+    float dy = player.pos.y - Boss->pos.y;
+    float dist = sqrtf(dx * dx + dy * dy);
+    if (dist > 0) {
+        Boss->pos.x += (dx / dist) * Boss->speed; // Normaliza direção
+        Boss->pos.y += (dy / dist) * Boss->speed;
+    }
+}
+
+bool ver_batida(Player *p, Vector2 posbot,int largurabot,int alturabot){
+    return(p->pos.x<posbot.x +largurabot && p->pos.x + PLAYER_SIZE > posbot.x &&
+p->pos.y < posbot.y + alturabot &&p->pos.y + PLAYER_SIZE > posbot.y);
 }
