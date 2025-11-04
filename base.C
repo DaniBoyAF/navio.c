e8d97b28f94c4c61a5febc47a89535aa95f2e5ca
Daniel <moongames12345@gmail.com>
2025-11-04 08:29:23 -0300

    troca de void pra o inimigos andarem

--- base.C
+++ base.C
@@ -2,13 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <math.h>
 #define WIDTH 800
 #define HEIGHT 450
 #define PLAYER_SIZE 32
 #define MAX_HP 3
+#define MAX_HP_BOSS 500
 
 typedef struct {
+    Vector2 pos;
     char nome[50];
     int score;
     int hp;
@@ -17,21 +19,25 @@ typedef struct {
     double tempo;
 }Player;
 typedef struct {
+    Vector2 pos;
     int X;
     int Y;
     int hp;
     int speed;
 }inimigo;
 typedef struct{
+    Vector2 pos;
     int X;
     int Y;
     int speed;
     int hp;
-}boss;
-void MoveIni(Inimigo *inimigo){
-    inimigo->y+=inimigo->speed;
-    if(inimigo->y > HEIGHT){
-        inimigo->y=0;
-        inimigo->x = GetRandomValue(0,WIDTH- 30);
+}Boss;
+void mover_em_direcao(Vector2 * origem,Vector destino,float speed ){
+    float dx= destino.X-origem->X;
+    float dy= destino.Y-origem->Y;
+    float dist=sqrt(dx*dx+ dy*dy)
+    if(dist>0){
+        origem->X= (dx/dist)*speed;
+        origem->y= (dy/dist)*speed;
     }
 }
